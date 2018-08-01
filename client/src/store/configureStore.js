ec79ab215b50d5b4bf6776c210063196cdf01630
Osman Ali <okali@uci.edu>
2018-08-01 15:59:57 -0700

    Cleanup

--- client/src/store/configureStore.js
+++ client/src/store/configureStore.js
@@ -8,8 +8,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default () => {
     const store = createStore(
         combineReducers({
-           notes: notesReducer,
-           selectedNote: selectedNoteReducer
+           // Reducers
         }),
         composeEnhancers(applyMiddleware(thunk))
     );
